3d1022a556413e98a1ae2cbff5381053962e16ee
Luqman Hakim <luqman.hakim.pens@gmail.com>
2019-01-12 23:22:53 +0700

    commit from github

--- lora_GPS_Compass_Board0/sloeber.ino.cpp
+++ lora_GPS_Compass_Board0/sloeber.ino.cpp
@@ -2,7 +2,7 @@
 //This is a automatic generated file
 //Please do not modify this file
 //If you touch this file your change will be overwritten during the next build
-//This file has been generated on 2019-01-11 23:45:43
+//This file has been generated on 2019-01-12 07:37:34
 
 #include "Arduino.h"
 #include "Arduino.h"
