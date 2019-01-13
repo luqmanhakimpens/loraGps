67befcac3c79eb99c53c5aa3714d9520b1bf423e
Luqman Hakim <luqman.hakim.pens@gmail.com>
2019-01-13 11:41:28 +0700

    add GPL v3 on lora_GPS_Compass_Board0 and Board1 files

--- lora_GPS_Compass_Board1/sloeber.ino.cpp
+++ lora_GPS_Compass_Board1/sloeber.ino.cpp
@@ -2,7 +2,7 @@
 //This is a automatic generated file
 //Please do not modify this file
 //If you touch this file your change will be overwritten during the next build
-//This file has been generated on 2019-01-12 02:03:02
+//This file has been generated on 2019-01-13 11:39:01
 
 #include "Arduino.h"
 #include <SPI.h>
